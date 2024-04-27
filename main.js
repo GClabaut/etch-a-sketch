65d1f471c548f1141f89b76bad4be4166de298b8
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-27 22:05:50 +0200

    Added function to change background color randomly on hover
    With querySelectorAll and forEach, we can add an event listener to each
    box, and in said event listener, we make sure to change the background
    color on hover, and to have a function that randomly defines the new color.

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ const container = document.querySelector('.container');
 
 function etch(size) {
     let boxSize = size;
-    let sizePercent = ( 1 / boxSize ) * 100;
+    let percentSize = ( 1 / boxSize ) * 100;
     
     const div = document.createElement('div');
     div.setAttribute('class', 'sketch');
-    div.style.flex = `0 0 ${sizePercent}%`
+    div.style.flex = `0 0 ${percentSize}%`;
     
     for (let i = 0; i < boxSize * boxSize; i++) {
         container.appendChild(div.cloneNode(true));
-    }
-}
+    };
+};
 
 etch(16);
+
+const boxes = document.querySelectorAll('.sketch');
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        box.style.background = `#${randomColor}`;
+    });
+});
