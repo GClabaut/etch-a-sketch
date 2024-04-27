7c1875a7ac9271e7d4579c3dd3da07f2aa1237c2
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-27 22:44:27 +0200

    Added working button to change box size
    When the button is clicked, a prompt asks a number from the user.
    If the result is not a number, or not between 1 and 100, the prompt loops.
    With this new number, we empty our container first with another while loops
    (in this loop, so long as container has a child, it will be removed until
    none are left). Then, we use the result from the prompt to call our previous
    etch function and create and new box with the prompted size and the event
    listener.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
 
 function etch(size) {
@@ -12,15 +13,28 @@ function etch(size) {
     for (let i = 0; i < boxSize * boxSize; i++) {
         container.appendChild(div.cloneNode(true));
     };
-};
 
-etch(16);
-
-const boxes = document.querySelectorAll('.sketch');
+    const boxes = document.querySelectorAll('.sketch');
 
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         let randomColor = Math.floor(Math.random()*16777215).toString(16);
         box.style.background = `#${randomColor}`;
     });
+});
+};
+
+etch(16);
+
+button.addEventListener('click', () => {
+    let result = prompt('Change the box size');
+    while (!Number.isInteger(parseInt(result)) || result <= 0 || 100 < result) {
+        result = prompt('Change the box size (must be between 1 and 100)');
+    }
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+
+    etch(result);
 });
